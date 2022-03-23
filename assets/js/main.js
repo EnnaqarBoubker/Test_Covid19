aec3a523acfc85c13c5954a1fb6ae1dbb2ede830
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-03-23 10:26:48 +0100

    ajouter une formulaire pour les facteur pronostique

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
+let buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", function(){
+        let isGreen = btn.classList.contains("btnClicked");
+        if(isGreen){
+            btn.classList.remove("btnClicked");
+        } else {
+            btn.classList.add("btnClicked");
+        }
+    });
+});
 
 
-let p1 = new Person(27, 72, 180);
+let btnTester = document.getElementById("btnTester");
 
+btnTester.addEventListener("click", function(e){
+    globalThis.obj = new Person(form['age'].value, form['age'].value, form['age'].value);
+})
--- assets/js/person.js
+++ assets/js/person.js
@@ -1,10 +1,19 @@
 class Person{
+    age;
+    piods;
+    taille;
+
+    answer = new Array();
+    f_pronostiques = new Array();
+    
+    f_graviteMineur = new Array();
+    f_graviteMajeur = new Array();
+
     constructor(age, poids, taille){
         this.age = age;
         this.poids = poids;
         this.taille = taille;
     }
-
-    answer = new Array();
-    f_gravite = new Array();
 }
+
+
