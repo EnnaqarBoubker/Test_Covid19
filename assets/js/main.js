ae1d3d8ef6aedada2e6760d0174301ae6ea7752a
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-03-23 22:07:32 +0100

    ajouter des methode pour remplir les tableaux de l'objet

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,9 +11,27 @@ buttons.forEach(btn => {
     });
 });
 
-
 let btnTester = document.getElementById("btnTester");
 
-btnTester.addEventListener("click", function(e){
-    globalThis.obj = new Person(form['age'].value, form['age'].value, form['age'].value);
-})
+
+btnTester.addEventListener("click", function(){
+    let testCovid19Form = document.forms['testCovid19'];
+    let facteurPronostique = document.forms['facteurPronostique'];
+    globalThis.obj = new Person(facteurPronostique['age'].value, facteurPronostique['poids'].value, facteurPronostique['taille'].value);
+    console.log(testCovid19Form.length);
+    console.log(Object.keys(testCovid19Form).length);
+    for(let i = 0 ; i < Object.keys(testCovid19Form).length ; i++){
+        obj.answers[testCovid19Form[i].name] = testCovid19Form[i].value;
+        console.log(testCovid19Form[i].name + " : " + testCovid19Form[i].value);
+    }
+
+    // Object.entries(testCovid19Form).forEach(item => {
+    //     console.log(item['fievre'].value);
+    // });
+    for(let i = 0 ; i < facteurPronostique.length ; i++){
+        obj.f_pronostiques[facteurPronostique[i].name] = facteurPronostique[i].value;
+    }
+
+    obj.getFacteurDeGraviteMajeur();
+    obj.getFacteurDeGraviteMineur();
+});
--- assets/js/person.js
+++ assets/js/person.js
@@ -3,7 +3,7 @@ class Person{
     piods;
     taille;
 
-    answer = new Array();
+    answers = new Array();
     f_pronostiques = new Array();
     
     f_graviteMineur = new Array();
@@ -13,6 +13,30 @@ class Person{
         this.age = age;
         this.poids = poids;
         this.taille = taille;
+    }
+
+    gravite(){
+        return this.f_graviteMajeur.length + this.f_graviteMineur.length;
+    }
+
+    getFacteurDeGraviteMajeur(){
+        if (parseFloat(this.answers['temperature']) <= 35.4 ){
+            this.f_graviteMajeur.push(this.answers['temperature']);
+        } else if (this.answers['geneRespiratoire'] == 'oui'){
+            this.f_graviteMajeur.push(this.answers['geneRespiratoire']);
+        } else if (this.answers['alimenterBoire'] == 'oui'){
+            this.f_graviteMajeur.push(this.answers['alimenterBoire']);
+        }
+    }
+
+    getFacteurDeGraviteMineur() {
+        if( parseFloat(this.answers['temperature']) >= 39){
+            this.f_graviteMineur['temperature'] = this.answers['temperature'];
+        } else if ( this.answers['fatigue'] == 'oui'){
+            this.f_graviteMineur['fatigue'] = this.answers['fatigue'] ;
+        } else if ( this.answers['senterVous'] == 'oui'){
+            this.f_graviteMineur['senterVous'] = this.answers['senterVous'];
+        }
     }
 }
 
