7cbe9de17e38d609d537e72785008a7d9fa10aca
elmehdinainia <mehdinainiaa4@gmail.com>
2022-03-25 11:27:15 +0100

    resoudre le probleme de progress bar

--- intro.js
+++ intro.js
@@ -3,11 +3,18 @@ const informations = document.querySelector("#informations");
 const questions = document.querySelector("#questions");
 
 // Events : 
-startTest.addEventListener("click", stepper);
+startTest.addEventListener("click", function() {
+  informations.classList.remove("label-circle-s");
+  console.log("eddd");
+  informations.classList.add("label-circle-ns");
+  questions.classList.remove("label-circle-ns");
+  questions.classList.add("label-circle-s");
+
+  });
 
 // functions : 
-function stepper() {
-informations.classList.remove("label-circle-s");
-informations.classList.add("label-circle-ns");
-  questions.classList.add("label-circle-s");
-}
+// function stepper() {
+// informations.classList.remove("label-circle-s");
+// informations.classList.add("label-circle-ns");
+// questions.classList.add("label-circle-s");
+// }
