ac1954c6891dc17680200bf1b40fe0d27a57268b
elmehdinainia <mehdinainiaa4@gmail.com>
2022-03-28 15:19:16 +0100

    ajouter nouveau fonction de progresse

--- intro.js
+++ intro.js
@@ -1,20 +1,51 @@
+console.log("js est liee")
+//todo page introduction
+
 const startTest = document.querySelector("#startest");
 const informations = document.querySelector("#informations");
 const questions = document.querySelector("#questions");
 
+//?---------------page questionaire--------------------
+const answers = document.querySelector("#answers");
+const info = document.querySelector("#info");
+
+answers.classList.remove("label-circle-ns");
+answers.classList.add("label-circle-s");
+
+info.classList.remove("label-circle-s");
+info.classList.add("label-circle-ns");
+
+
 // Events : 
-startTest.addEventListener("click", function() {
+//!______________________arrow function ___________________________________
+   startTest.addEventListener("click", () => {
   informations.classList.remove("label-circle-s");
-  console.log("eddd");
   informations.classList.add("label-circle-ns");
   questions.classList.remove("label-circle-ns");
   questions.classList.add("label-circle-s");
 
-  });
 
-// functions : 
-// function stepper() {
-// informations.classList.remove("label-circle-s");
-// informations.classList.add("label-circle-ns");
-// questions.classList.add("label-circle-s");
-// }
+
+  });
+//!______________________function annonyme___________________________________
+
+  // startTest.addEventListener("click", function(){
+  //   informations.classList.remove("label-circle-s");
+  //   console.log("eddd");
+  //   informations.classList.add("label-circle-ns");
+  //   questions.classList.remove("label-circle-ns");
+  //   questions.classList.add("label-circle-s");
+  
+  //   });
+
+
+  
+//   const resultats = document.querySelector("#result");
+//   const btntst = document.querySelector("#btnSuivant");
+
+//   btnSuivant.addEventListener("click", ()=>{
+//   answers.classList.remove("label-circle-ns");
+//   answers.classList.remove("label-circle-s");
+//   resultats.classList.add("label-circle-s");
+//   resultats.classList.add("label-circle-ns");
+// });
