79182df3f10e1e890c57a3741e6888b3eacb5ebf
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-03-28 22:17:12 +0100

    just pour faire un merge

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ function testCovide19(obj) {
                 alert(msg);
             } else if ((obj.gravite() == 0 && (obj.age > 50 || obj.age <= 69)) || (obj.graviteMineur() >= 1)) {
                 //console.log(`téléconsultation ou médecin généraliste ou visite à domicile`);
-                let msg = `téléconsultation ou médecin généraliste ou visite à domicile`;
+                let msg = `téléconsultation ou médecin généraliste ou visite à domicile “appelez le 141 si une gêne respiratoire ou des difficultésimportantes pours’alimenter ou boire pendant plus de 24h apparaissent.`;
                 alert(msg);
             }
         } else if (obj.pronostique() >= 1) {
             if (obj.gravite() == 0) {
-                console.log(`téléconsultation ou médecin généraliste ou visite à domicile`);
+                console.log(`téléconsultation ou médecin généraliste ou visite à domicile “appelez le 141 si une gêne respiratoire ou des difficultésimportantes pours’alimenter ou boire pendant plus de 24h apparaissent.`);
             } else if (obj.graviteMineur() == 1 && obj.graviteMajeur() == 0) {
                 console.log(`téléconsultation ou médecin généraliste ou visite à domicile`);
             } if (obj.graviteMineur() >= 2 && obj.graviteMajeur() == 0) {
