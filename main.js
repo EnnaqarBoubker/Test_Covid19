ac93e2ae3e4221f82d392acd89afc8bfd6edaeaf
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-03-28 15:33:52 +0100

    validé tous les algorithme de boubker

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function testCovide19(obj) {
-    if (obj.boubker() == true) {
+    if (obj.boubker() == true && obj.graviteMajeur() == 0) {
         if (obj.pronostique() == 0) {
             if (obj.gravite() == 0 && obj.age < 50) {
                 //console.log(`rester à votre domicile et de contacter votre médecin en cas d’ apparition de nouveaux symptômes.Vous pourrez aussi utiliser à nouveau l’ application pour réévaluer vos symptômes.`)
@@ -10,16 +10,16 @@ function testCovide19(obj) {
                 let msg = `téléconsultation ou médecin généraliste ou visite à domicile`;
                 alert(msg);
             }
-        } else if (obj.pronostique >= 1) {
+        } else if (obj.pronostique() >= 1) {
             if (obj.gravite() == 0) {
                 console.log(`téléconsultation ou médecin généraliste ou visite à domicile`);
-            } else if (obj.graviteMineur() >= 1) {
-                console.log(`téléconsultation ou médecin généraliste ou visite à domicile jjjjjjj`);
-            } else if (obj.graviteMineur() >= 2) {
-                console.log(`Appele 414`);
+            } else if (obj.graviteMineur() == 1 && obj.graviteMajeur() == 0) {
+                console.log(`téléconsultation ou médecin généraliste ou visite à domicile`);
+            } if (obj.graviteMineur() >= 2 && obj.graviteMajeur() == 0) {
+                console.log(`Appele 141`);
             }
-        } else if (obj.graviteMajeur() >= 1) {
-            console.log(`appele 233`);
         }
+    } else if (obj.boubker() == true && obj.graviteMajeur() >= 1 ) {
+            console.log(`appele 141`);
     }
 }
