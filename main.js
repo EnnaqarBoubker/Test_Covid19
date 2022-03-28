e0f039ebfdfe5454d78902b714f83ed8ab26e7f2
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-03-28 22:25:13 +0100

    ajouter le msg de resultat + liee les pages entre elles

--- main.js
+++ main.js
@@ -4,22 +4,32 @@ function testCovide19(obj) {
             if (obj.gravite() == 0 && obj.age < 50) {
                 //console.log(`rester à votre domicile et de contacter votre médecin en cas d’ apparition de nouveaux symptômes.Vous pourrez aussi utiliser à nouveau l’ application pour réévaluer vos symptômes.`)
                 let msg = "nous vous conseillons de rester à votre domicile et de contacter votre médecin en cas d’apparition de nouveaux symptômes. Vous pourrez aussi utiliser à nouveau l’application pour réévaluer vos symptômes";
-                alert(msg);
+                //alert(msg);
+                document.getElementById('msg').innerText = msg;
             } else if ((obj.gravite() == 0 && (obj.age > 50 || obj.age <= 69)) || (obj.graviteMineur() >= 1)) {
                 //console.log(`téléconsultation ou médecin généraliste ou visite à domicile`);
                 let msg = `téléconsultation ou médecin généraliste ou visite à domicile “appelez le 141 si une gêne respiratoire ou des difficultésimportantes pours’alimenter ou boire pendant plus de 24h apparaissent.`;
-                alert(msg);
+                //alert(msg);
+                document.getElementById('msg').innerText = msg;
             }
         } else if (obj.pronostique() >= 1) {
             if (obj.gravite() == 0) {
-                console.log(`téléconsultation ou médecin généraliste ou visite à domicile “appelez le 141 si une gêne respiratoire ou des difficultésimportantes pours’alimenter ou boire pendant plus de 24h apparaissent.`);
+                //console.log(`téléconsultation ou médecin généraliste ou visite à domicile “appelez le 141 si une gêne respiratoire ou des difficultésimportantes pours’alimenter ou boire pendant plus de 24h apparaissent.`);
+                let msg = "téléconsultation ou médecin généraliste ou visite à domicile “appelez le 141 si une gêne respiratoire ou des difficultésimportantes pours’alimenter ou boire pendant plus de 24h apparaissent.";
+                document.getElementById('msg').innerText = msg;
             } else if (obj.graviteMineur() == 1 && obj.graviteMajeur() == 0) {
-                console.log(`téléconsultation ou médecin généraliste ou visite à domicile`);
+                //console.log(`téléconsultation ou médecin généraliste ou visite à domicile`);
+                let msg = `téléconsultation ou médecin généraliste ou visite à domicile`;
+                document.getElementById('msg').innerText = msg;
             } if (obj.graviteMineur() >= 2 && obj.graviteMajeur() == 0) {
-                console.log(`Appele 141`);
+                //console.log(`Appele 141`);
+                let msg = `Appele 141`;
+                document.getElementById('msg').innerText = msg;
             }
         }
     } else if (obj.boubker() == true && obj.graviteMajeur() >= 1 ) {
-            console.log(`appele 141`);
+            //console.log(`appele 141`);
+            let msg = `Appele 141`;
+            document.getElementById('msg').innerText = msg;
     }
 }
